68ade4fabed76035c639c21f25b28e5240266e13
Shubham Katara <shubham.katara@trivago.com>
2024-03-15 17:52:53 +0100

    change as needed to work in PATH env var

--- resource.py
+++ resource.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import argparse
 from dotenv import load_dotenv
 from yaml import safe_load
@@ -7,13 +8,13 @@ from sys import exit
     
 def nodeComputeUsage():
 ##Check pods on the node
-    if args.namespace is None:
-        pods_on_node = popen(f"kubectl get pods --field-selector=spec.nodeName={args.node},status.phase=Running -A -o yaml 2> /dev/null").read()
+    if args.ns is None:
+        pods_on_node = popen(f"kubectl get pods --field-selector=spec.nodeName={args.n},status.phase=Running -A -o yaml 2> /dev/null").read()
     else:
-        pods_on_node = popen(f"kubectl get pods --field-selector=spec.nodeName={args.node},status.phase=Running  -n {args.namespace} -o yaml 2> /dev/null").read()
+        pods_on_node = popen(f"kubectl get pods --field-selector=spec.nodeName={args.n},status.phase=Running  -n {args.ns} -o yaml 2> /dev/null").read()
     pod_dict = safe_load(pods_on_node)
     final = []
-    print(f"\nShowing Resource Consumption on {args.node}. Memory shown is in Mi. \n") if args.namespace is None else print(f"\nShowing Resource Consumption on {args.node} and for Namespace {args.namespace}. Memory shown is in Mi. \n")
+    print(f"\nShowing Resource Consumption on {args.n}. Memory shown is in Mi. \n") if args.ns is None else print(f"\nShowing Resource Consumption on {args.n} and for Namespace {args.ns}. Memory shown is in Mi. \n")
     print(f"======================================================================\n")
     for data in pod_dict['items']:
         top = f'kubectl top pod -n {data["metadata"]["namespace"]} {data["metadata"]["name"]} --no-headers 2> /dev/null'
@@ -24,7 +25,7 @@ def nodeComputeUsage():
             mem_usage = pod_split_data[2].split("Mi")
         pod_data = {
                 "Memory": mem_usage[0],
-                **({'Namespace': data["metadata"]["namespace"]} if args.namespace is  None else {}),  #Read more about "dictionary unpacking"
+                **({'Namespace': data["metadata"]["namespace"]} if args.ns is  None else {}),  #Read more about "dictionary unpacking"
                 "PodName": data["metadata"]["name"],
                 # "CPU": cpu_usage,
             }
@@ -35,16 +36,16 @@ def nodeComputeUsage():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--node", help="Node to check the usage for")
-    parser.add_argument("--namespace", help="Namespace to check the usage for")
-    parser.add_argument("--cluster", help="Cluster to check for [prod,stage]")
+    parser.add_argument("-n", help="Node to check the usage for")
+    parser.add_argument("-ns", help="Namespace to check the usage for")
+    parser.add_argument("-c", help="Cluster to check for [prod,stage]")
     args = parser.parse_args()
-    if args.cluster == "prod" or args.cluster == "Prod":
-        environ['KUBECONFIG'] = "./kube_prod"
+    if args.c == "prod" or args.c == "Prod":
+        environ['KUBECONFIG'] = "/Users/skatara/.kube/kube_prod"
     else:
-        environ['KUBECONFIG'] = "./kube_stage"
+        environ['KUBECONFIG'] = "/Users/skatara/.kube/kube_stage"
     #Exit if node name not given
-    if not args.node:
+    if not args.n:
         print("Provide the node name via --node option")
         exit()
     nodeComputeUsage()
